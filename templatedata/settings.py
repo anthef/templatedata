892180f15ab90a6adefe64f6f73a368427845d5c
anthef <anthonyef09@gmail.com>
2025-05-02 00:00:08 +0700

    dv

--- templatedata/settings.py
+++ templatedata/settings.py
@@ -87,7 +87,10 @@ WSGI_APPLICATION = 'templatedata.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"), conn_max_age=600)
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
