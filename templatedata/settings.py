ca2bc264ff0ade0efa9d156b962723a3fc0a4fd6
anthef <anthonyef09@gmail.com>
2025-05-01 23:57:39 +0700

    apasi

--- templatedata/settings.py
+++ templatedata/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'templatedata.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-  'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"), conn_max_age=600)
+    'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"), conn_max_age=600)
 }
 
 
